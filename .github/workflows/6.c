df530748ae5dc301b7aa4a0bb553d4430261d7fe
Ahmad Maruf Hossain <55588641+SpicyFalcon619@users.noreply.github.com>
2024-12-04 22:37:44 +0600

    6.c

--- .github/workflows/6.c
+++ .github/workflows/6.c
@@ -5,7 +5,7 @@
 int main(){
     int n;
     scanf("%d", &n);
-    int a[n], b[n];
+    int a[n], b[n], c[n];
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
@@ -16,7 +16,11 @@ int main(){
     }
     for (int i = 0; i < n; i++)
     {
-        printf("%d ", a[i] + b[i]);
+        c[i] = a[i] + b[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", c[i]);
     }
 
     return 0;
